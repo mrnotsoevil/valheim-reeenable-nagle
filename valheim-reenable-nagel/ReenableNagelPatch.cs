7f5d4e62ff986d40285e5cd4c7301430901b80e5
Ruman Gerst <ruman.gerst@leibniz-hki.de>
2021-02-28 15:41:07 +0100

    * Initial commit

---
+++ valheim-reenable-nagel/ReenableNagelPatch.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using HarmonyLib;
+using Steamworks;
+
+namespace valheim_reenable_nagel
+{
+    [HarmonyPatch(typeof(ZSteamSocket), "SendQueuedPackages")]
+    public class ReenableNagelPatch
+    {
+        static bool Prefix(ZSteamSocket ___instance, Queue<byte[]> ___m_sendQueue, ref int ___m_totalSent, HSteamNetConnection ___m_con)
+        {
+            if (!___instance.IsConnected())
+                return false;
+            while (___m_sendQueue.Count > 0)
+            {
+                byte[] source = ___m_sendQueue.Peek();
+                IntPtr num = Marshal.AllocHGlobal(source.Length);
+                Marshal.Copy(source, 0, num, source.Length);
+                // This was k_nSteamNetworkingSend_ReliableNoNagle before
+                EResult connection = SteamNetworkingSockets.SendMessageToConnection(___m_con, num, (uint) source.Length, Constants.k_nSteamNetworkingSend_Reliable, out long _);
+                Marshal.FreeHGlobal(num);
+                if (connection == EResult.k_EResultOK)
+                {
+                    ___m_totalSent += source.Length;
+                    ___m_sendQueue.Dequeue();
+                }
+                else
+                {
+                    ZLog.Log((object) ("Failed to send data " + (object) connection));
+                    break;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ valheim-reenable-nagel/ValheimReenableNaglePlugin.cs
@@ -0,0 +1,16 @@
+﻿using BepInEx;
+using HarmonyLib;
+using UnityEngine;
+
+namespace valheim_reenable_nagel
+{
+    [BepInPlugin("de.mrnotsoevil.valheim.reenable-nagel", "Valheim Re-enable Nagel's Algorithm", "1.0.0.0")]
+    public class ValheimReenableNaglePlugin : BaseUnityPlugin
+    {
+        public void Awake()
+        {
+            Debug.Log("[Valheim Show Me!] Making player show on map by default ...");
+            Harmony.CreateAndPatchAll(typeof(ReenableNagelPatch));
+        }
+    }
+}
